a5f090e5295a19b421111c3d12d145930a62191b
Caroline Urbainski <caroline.urbainski@hotmail.com>
2024-04-29 12:05:06 -0300

    database

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCaX0xUxxA4CGZi21-oTiJL6O1N5ouzCAg",
+    authDomain: "site-dreamfyre.firebaseapp.com",
+    projectId: "site-dreamfyre",
+    storageBucket: "site-dreamfyre.appspot.com",
+    messagingSenderId: "638732952802",
+    appId: "1:638732952802:web:20e2404d944f6158d66abb"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  var infoRef = firebase.database().ref('infos')
+
 $(document).ready(function(){
     console.log($('.btn'))
    $('#tel').mask('(00)00000-0000');
@@ -5,11 +17,28 @@ $(document).ready(function(){
 
 $('form').on('submit',function(e){
     e.preventDefault();
-    var name = getInputVal('nome') 
-    var email = getInputVal('email') 
-
-
+    var name = $('#nome').val();
+    var email = $('#email').val();
+    var telephone = $('#tel').val();
+    var message = $('#mensagem').val();
+   saveInfo(name,email,telephone,message);
     $('input').val('');
     
 })
+ function saveInfo(name, email, telephone, message)
+ {
+    var newInfoRef= infoRef.push();
+    newInfoRef.set(
+        {
+            nome: name,
+            email: email,
+            telefone:telephone,
+            mensagem: message
+
+        }
+
+
+    )
+
+ }
 
