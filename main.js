760beabf3afda616a086589a920a7738a9f9f5d3
Caroline Urbainski <caroline.urbainski@hotmail.com>
2024-04-29 11:09:53 -0300

    atualização

--- main.js
+++ main.js
@@ -5,24 +5,11 @@ $(document).ready(function(){
 
 $('form').on('submit',function(e){
     e.preventDefault();
+    var name = getInputVal('nome') 
+    var email = getInputVal('email') 
+
+
     $('input').val('');
+    
 })
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCaX0xUxxA4CGZi21-oTiJL6O1N5ouzCAg",
-  authDomain: "site-dreamfyre.firebaseapp.com",
-  projectId: "site-dreamfyre",
-  storageBucket: "site-dreamfyre.appspot.com",
-  messagingSenderId: "638732952802",
-  appId: "1:638732952802:web:20e2404d944f6158d66abb"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
